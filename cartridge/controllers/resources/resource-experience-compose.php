8b6ae349789c4ac0189e742b04f57734254cbec9
adolphusnolan <adolphusnolan@gmail.com>
2020-05-21 20:25:48 -0500

    /compose with CI/CD
    //thread not found error addressed

--- cartridge/controllers/resources/resource-experience-compose.php
+++ cartridge/controllers/resources/resource-experience-compose.php
@@ -53,7 +53,7 @@
 
             //
             $request['thread'] = $thread_details['data'][0]['id'];
-            echo json_encode($request); exit;
+            //echo json_encode($request); exit;
 
             $request['domain'] = 'message';
 
