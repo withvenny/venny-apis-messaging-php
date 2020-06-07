0c8ee0e349958c47926a2e15558cb55800e23234
adolphusnolan <adolphusnolan@gmail.com>
2020-06-07 15:22:23 -0500

    participants search
    json_decode, echo participants trying -> var_dump

--- cartridge/models/classes/class-messaging.php
+++ cartridge/models/classes/class-messaging.php
@@ -377,7 +377,7 @@
 
                     $participants = json_decode($request['participants']);
                     echo "ONE" . $participants;
-                    echo "TWO" . json_decode($participants->contributor);
+                    echo "TWO" . var_dump($participants->contributor);
 
                     $conditions.= ' WHERE ';
                     $conditions.= 'thread_participants ->\'contributors\' @> \''. $participants . '\'::jsonb';
