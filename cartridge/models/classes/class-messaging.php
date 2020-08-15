c92cb577f50cb99f9c22302c88882aebf0d81328
adolphusnolan <adolphusnolan@gmail.com>
2020-08-14 21:53:53 -0500

    making external API call as test

--- cartridge/models/classes/class-messaging.php
+++ cartridge/models/classes/class-messaging.php
@@ -478,7 +478,7 @@
                     $api = "https://io-venny-api.herokuapp.com/profiles?app=app_thentrlco&token=tkn_thentrlco&profile=prf_8072738b47905&id=prf_8072738b47905";
 
                     $json = file_get_contents($api);
-                    $json = json_decode($json['data'][0],true);
+                    $json = json_decode($json,true);
 
                     $code = 200;
                     $message = "OK";
@@ -511,7 +511,7 @@
                     'total' => $total
                 ],
                 'data' => $data,
-                'json' => $json,
+                'json' => $json['data'][0],
                 'log' => [
                     'process' => $process_id = $this->token->process_id(),
                     'event' => $event_id = $this->token->event_id($process_id)
