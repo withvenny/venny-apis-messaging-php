f6a0589f330c87a779d3ef8d02cc97685de1c0fa
adolphusnolan <adolphusnolan@gmail.com>
2020-08-21 20:17:35 -0500

    /profile microservice
    debugging a bit

--- cartridge/models/classes/class-messaging.php
+++ cartridge/models/classes/class-messaging.php
@@ -501,7 +501,7 @@
 
                             $api = "https://io-venny-api.herokuapp.com/profiles?app=app_thentrlco&token=tkn_thentrlco&profile=prf_8072738b47905&id=" . $contributor;
                             $profile = file_get_contents($api);
-                            $profile = json_encode($profile,true);
+                            $profile = json_decode($profile,true);
 
                             echo var_dump($profile);
                             exit;
