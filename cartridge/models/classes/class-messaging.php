c8713aba18a4fa9fc542640f2b7c5b58a7488b21
adolphusnolan <adolphusnolan@gmail.com>
2020-08-30 23:32:13 -0500

    microservice
    data display

--- cartridge/models/classes/class-messaging.php
+++ cartridge/models/classes/class-messaging.php
@@ -528,6 +528,9 @@
                             echo json_encode($profile['message']);
                             echo json_encode($profile['log']['process']);
                             echo json_encode($profile['data'][0]);
+                            echo json_encode($profile['data'][0]['id']);
+                            echo json_encode($profile['data'][0]['alias']);
+                            echo json_encode($profile['data'][0]['images']);
                             exit;
 
                             $contributor_id=$profile[0]->data->id;
